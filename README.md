f2314a934c488813411f853368ed4ac441efd27c
Elphas <elphastori@gmail.com>
2019-05-12 02:18:25 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# bitcoin-whatsapp-notifier
---
+++ index.js
@@ -0,0 +1,51 @@
+const axios = require("axios");
+
+const accountSid = "AC9de9cb9ae046a09717525d23d1b3eae7";
+const authToken = "6ba03c17c8883deeec04ea57d0f96f59";
+const twilio = require('twilio')(accountSid, authToken);
+
+const increment = 1000;
+let lastAsk = 0;
+let lastBid = 0;
+
+const getBitcoinTicker = async () => {
+    try {
+        const ticker = await axios.get("https://api.mybitx.com/api/1/ticker?pair=XBTZAR");
+        console.log(ticker.data);
+        return ticker.data;
+    } catch(error) {
+        console.error(error);
+    }
+}
+
+const sendWhatsappUpdate = async (ask, bid) => {
+    try {
+        const response = await twilio.messages.create({
+            from: "whatsapp:+14155238886",
+            body: `Your appointment is coming up on ${ask} at to ${bid}`,
+            to: "whatsapp:+27608917254"});
+        console.log(response);
+    } catch(error) {
+        console.error(error);
+    }
+}
+
+const monitorBitcoin = async () => {
+    const ticker = await getBitcoinTicker();
+    const ask = parseFloat(ticker.ask);
+    const bid = parseFloat(ticker.bid);
+
+    if (Math.abs(ask - lastAsk) > increment || Math.abs(bid - lastBid) > increment) {
+        if (Math.abs(ask - lastAsk) > increment) {
+            lastAsk = ask;
+        }
+
+        if (Math.abs(bid - lastBid) > increment) {
+            lastBid = bid;
+        }
+
+        await sendWhatsappUpdate(ask, bid);
+    }
+}
+
+setInterval(monitorBitcoin, 10000);
